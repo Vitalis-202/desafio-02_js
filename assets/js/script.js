b2aab778b074bff5eb56acc13101051d90e1a103
VitaDev <pablo.perez.lee@gmail.com>
2024-12-02 12:28:56 -0300

    Segundo ejercicio

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,20 @@ btn_ej1.addEventListener('click',()=>{
         contador--
     }
 })
+//Ejercicio #02
+let input_1 = document.querySelector('#input_1')
+let input_2 = document.querySelector('#input_2')
+let input_3 = document.querySelector('#input_3')
+const btn_ej2 = document.querySelector('#btn_ej2')
+let resultado = document.querySelector('#resultado_ej2')
+let suma = 0
+btn_ej2.addEventListener('click',()=>{
+    suma = Number(input_1.value) + Number(input_2.value) + Number(input_3.value)
+    console.log(suma)
+    if ( suma <= 10) {
+        resultado.innerHTML = 'Llevas ' + suma + ' stickers.'  
+    } else {
+        resultado.innerHTML = 'Llevas demasiados stickers.'  
+    }
+    suma = 0
+})
